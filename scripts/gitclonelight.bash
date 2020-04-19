3ff90bbbd4164df4d06f550334dc809a9813961a
andreabonetti <andreabonetti@gmail.com>
2020-04-19 19:16:55 +0200

    + gitclonelight

---
+++ scripts/gitclonelight.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# gitclonelight
+#
+# Description: update existing git folder by cloning
+# without the whole git history (--depth=1).
+# So you avoid downloading large amount of data.
+#
+# Usage:
+# ./gitclonelight.bash git_repo_folder_path
+# where git_repo_folder_path is the path of the
+# git repository folder
+#
+
+# get input variable
+FOLD=$1
+
+# get git url
+pushd $FOLD
+URL=$(git remote get-url origin)
+popd
+
+# remove folder
+rm -rf $FOLD
+
+# git clone without history
+git clone --depth=1 $URL
