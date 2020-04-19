791e52543ecba73ed085d56baa48694f45e9ef36
andreabonetti <andreabonetti@gmail.com>
2020-04-19 19:43:16 +0200

    + aliases

---
+++ config/aliases.bash
@@ -0,0 +1,31 @@
+
+# list (ls)
+alias l='ls -l'
+alias ll='ls -la'
+alias lr='ls -ltr'
+alias lh='ls -lh'
+
+# tar
+# usage: targz archive-name.tar.gz file-name
+# usage: targz archive-name.tar.gz directory-name
+alias targz='tar -zcvf'
+alias untargz='tar -zxvf'
+alias untar='tar -xvf'
+
+# applications
+alias sc='screen'
+
+### git
+alias gits='git status'
+alias gitf='git fetch'
+alias gitr='git rebase'
+alias gitc='git commit'
+alias gitp='git push'
+alias gita='git add'
+alias giturl='git config --get remote.origin.url'
+alias gitls='git ls-files'
+alias gitclonelight='source $PATH_CLIA_SCRIPTS/gitclonelight.bash'
+
+# misc
+alias ..='cd ..'
+
