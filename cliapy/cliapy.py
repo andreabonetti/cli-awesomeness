e34ca5312ac5400181c61030765058d98b41d384
Andrea Bonetti <andreabonetti@gmail.com>
2026-03-15 19:59:38 +0100

    removing cliapy, now that dedicated repo exists

--- cliapy/cliapy.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-import os
-
-def add_date(file):
-    """Add date prefix to file"""
-    today = date.today().strftime("%Y_%m_%d")
-    os.system(f"mv {file} {today}_{file}")
--- scripts/add_date.py
+++
@@ -1,7 +0,0 @@
-"""add_time"""
-
-from cliapy import *
-import sys
-
-if __name__ == '__main__':
-    add_date(sys.argv[1])
